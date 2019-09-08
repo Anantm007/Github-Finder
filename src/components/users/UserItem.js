c8b076a809970ffca8b6b5fbd72658ec9d2c8989
anantm007 <anant.mathur007@gmail.com>
2019-09-08 21:37:27 +0530

    HTTP request to github

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const UserItem = ({user: {login, avatar_url, html_url}}) => {
     
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,32 +3,10 @@ import UserItem from './UserItem';
 
 export class Users extends Component {
 
-    state= {
-        users: [
-            {
-                "login": "defunkt",
-                "id": 2,
-                "avatar_url": "https://avatars0.githubusercontent.com/u/2?v=4",
-                "html_url": "https://github.com/defunkt",
-                },
-                {
-                "login": "pjhyett",
-                "id": 3,
-                "avatar_url": "https://avatars0.githubusercontent.com/u/3?v=4",
-                "html_url": "https://github.com/pjhyett",
-                },
-                {
-                "login": "wycats",
-                "id": 4,
-                "avatar_url": "https://avatars0.githubusercontent.com/u/4?v=4",
-                "html_url": "https://github.com/wycats",
-                }
-        ]
-    }
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem key={user.id} user={user}></UserItem>
                 ))} 
                 
