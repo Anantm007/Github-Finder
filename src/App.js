d43ed5d61355d10e59faad9f2dfd7f0839b5f8e6
anantm007 <anant.mathur007@gmail.com>
2019-09-09 15:56:54 +0530

    Passing props and search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {Component, Fragment} from 'react';
+import React,{Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 
 import './App.css';
@@ -11,13 +12,16 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
-    this.setState({loading: true})
-   
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+ 
+  // Search Github Users
+  searchUsers = async (text) => {
+    this.setState({loading: true});
+    
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({users:res.data, loading: false})
+    this.setState({users:res.data.items, loading: false})
   }
+
 
   render() {
 
@@ -26,6 +30,7 @@ class App extends Component {
         <Navbar title="Github Finder" icon="fab fa-github"/>
         
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
         <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
