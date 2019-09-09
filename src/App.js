881f35e06b973e629bba254f18a5684ccd641328
anantm007 <anant.mathur007@gmail.com>
2019-09-09 16:15:42 +0530

    Clear users from state

--- src/App.js
+++ src/App.js
@@ -16,26 +16,31 @@ class App extends Component {
   // Search Github Users
   searchUsers = async (text) => {
     this.setState({loading: true});
-    
+
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({users:res.data.items, loading: false})
   }
 
+  // Clear users from state
+  clearUsers = () => {
+    this.setState({users: [], loading: false});
+  }
 
   render() {
+    const {users, loading} = this.state;
 
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github"/>
         
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-        <Users loading={this.state.loading} users={this.state.users}/>
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false}/>
+        <Users loading={loading} users={users}/>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App; 
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ export class Search extends Component {
     }
     
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
 
     onSubmit = (e) => {
@@ -20,12 +22,16 @@ export class Search extends Component {
         this.setState({text: e.target.value});
     }
     render() {
+        const {showClear, clearUsers} = this.props;
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
                     <input type="text" name="text" placeholder="Search Users.." value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+               {this.props.showClear && (
+                   <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
+               )} 
             </div>
         )
     }
