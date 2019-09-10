facabde49ba3cf5472859318c86e7f464c16339f
anantm007 <anant.mathur007@gmail.com>
2019-09-10 15:35:08 +0530

    Alert Context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
   
-import React, { Fragment, useState } from 'react';
+import React, { Fragment} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -9,34 +9,27 @@ import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => setAlert(null), 5000);
-  };
 
   return (
     <GithubState>
+      <AlertState>
 <Router>
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Alert alert={alert} />
+          <Alert/>
           <Switch>
             <Route
               exact
               path='/'
               render={props => (
                 <Fragment>
-                  <Search
-                    setAlert={showAlert}
-                  />
+                  <Search/>
                   <Users />
                 </Fragment>
               )}
@@ -51,6 +44,7 @@ const App = () => {
         </div>
       </div>
     </Router>
+    </AlertState>
     </GithubState>
     
   );
