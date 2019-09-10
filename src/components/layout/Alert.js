e796024d360624ffa8c13024cd338c25f12de778
anantm007 <anant.mathur007@gmail.com>
2019-09-10 15:39:29 +0530

    Fix alert

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,18 +1,18 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react';
 import AlertContext from '../../context/alert/alertContext';
 
 const Alert = () => {
-    const alertContext = useContext(AlertContext);
+  const alertContext = useContext(AlertContext);
 
-    const {alert} = alertContext;
-    
-    return (
-        alert!==null && (
-            <div className={`alert alert-${alert.type}`}>
-                <i className="fa fa-info-circle"></i> {alert.msg}
-            </div>
-        )
+  const { alert } = alertContext;
+
+  return (
+    alert !== null && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className='fas fa-info-circle' /> {alert.msg}
+      </div>
     )
-}
+  );
+};
 
-export default Alert
+export default Alert;
