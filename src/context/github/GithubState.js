410bee3c9efe268ecadbc07535c1ca178a70cd77
anantm007 <anant.mathur007@gmail.com>
2019-09-10 15:11:58 +0530

    Repos context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -56,7 +56,21 @@ const GithubState = props => {
   
 
     // Get repos
+    const getUserRepos = async username => {
+     
+      const res = await axios.get(
+        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+          process.env.REACT_APP_GITHUB_CLIENT_ID
+        }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
 
+      dispatch({
+        type: GET_REPOS,
+        payload: res.data
+      })
+    };
+  
+  
     // Clear users
     const clearUsers = () => dispatch({type: CLEAR_USERS});
     // Set loading
@@ -71,7 +85,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
     }}
     >
         {props.children}
