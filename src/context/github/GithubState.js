f41c70ca31f1f7d7db0340fc632682b48a490b24
anantm007 <anant.mathur007@gmail.com>
2019-09-10 16:48:46 +0530

    Deploy to netlify

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -11,6 +11,21 @@ import {
     GET_USER
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production')
+{
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} 
+
+else
+{
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = props => {
     const intitialState = {
         users: [],
@@ -27,8 +42,8 @@ const GithubState = props => {
     
         const res = await axios.get(
           `https://api.github.com/search/users?q=${text}&client_id=${
-            process.env.REACT_APP_GITHUB_CLIENT_ID
-          }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+            githubClientId
+          }&client_secret=${githubClientSecret}`
         );
     
         dispatch({
@@ -44,8 +59,8 @@ const GithubState = props => {
   
       const res = await axios.get(
         `https://api.github.com/users/${username}?client_id=${
-          process.env.REACT_APP_GITHUB_CLIENT_ID
-        }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+          githubClientId
+        }&client_secret=${githubClientSecret}`
       );
         
       dispatch({
