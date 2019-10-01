f75b004f63f3fc0c3b907ae2a0443e9764ba787c
Anant Mathur <anant.mathur007@gmail.com>
2019-10-01 18:37:55 +0530

    Update NotFound.js

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFound = () => {
     return (
         <div>
-            <h1>404 Page Not Found..</h1>
+            <h1>404 Page Not Found :(</h1>
             <p className="lead">The page you are looking for does not exist</p>
         </div>
     )
